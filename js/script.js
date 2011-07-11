b9851f2caf03a4495dc710fc117f42ac13a5fab2
Ægir Þorsteinsson <aegir@thorsteinsson.is>
2011-07-11 19:59:32 +0000

    added new pictures to slider and fixed menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-/* Author: 
-
-*/
-
 $(function() {
     $('#slider').nivoSlider({
     	controlNav: false
     });
     
     $('.menu :not(.right) a').bind('click', function(e) {
-    	$(this).parent().addClass('selected').siblings().removeClass('selected');
+		$(".menu li:not(.right)").removeClass("selected");
+		$(this).parent().addClass('selected');
     	var elem = $($(this).attr('href'));
     	if (elem.hasClass('visuallyhidden')) {
 	    	elem.siblings().filter(':not(.visuallyhidden)').slideUp(300, function() {
@@ -22,15 +19,13 @@ $(function() {
     $('.i18n a').bind('click', function(e) {
     	var elem = $(this);
     	if (elem.attr('href').indexOf('en') > 0) {
-    		$('.is').fadeOut(200, function(){
-    			$('.en').fadeIn(200);
-    			elem.parent().hide().next().show();
-    		});
+    		$('.is').hide();
+    		$('.en').show();
+    		elem.parent().hide().next().show();
     	} else {
-    		$('.en').fadeOut(200, function(){
-    			$('.is').fadeIn(200);
-    			elem.parent().hide().prev().show();
-    		});
+    		$('.en').hide();
+    		$('.is').show();
+    		elem.parent().hide().prev().show();
     	}
     	e.preventDefault();
     });
